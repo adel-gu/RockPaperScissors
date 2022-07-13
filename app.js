47bdaa879d775a086c5c9c7765f71fcc5b98ccdf
adelguit <adeelguitoun@gmail.com>
2022-07-13 19:19:54 +0100

    One round game playing.

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 // choose randomly between ['Rock', 'Paper', 'Scissors']
 const HAND = ["Rock", "Paper", "Scissors"];
 
-let ComputerSelection = computerPlay();
-console.log(ComputerSelection);
+const computerSelection = computerPlay();
+const playerSelection = "Rock";
+
+// ? Main Function
+console.log(playRound(playerSelection, computerSelection));
 
 // ! Function Declarations
 function computerPlay() {
   let index = Math.floor(Math.random() * 3);
-  // console.log(HAND[index]);
-  return HAND[index];
+  return index;
+}
+
+function playRound(pSelection, cSelection) {
+  pIndex = HAND.indexOf(pSelection);
+  switch (pIndex - cSelection) {
+    case 0:
+      return "That's a Tie!!";
+      break;
+    case 1:
+      return `You Win, ${HAND[pIndex]} beats ${HAND[cSelection]}.`;
+      break;
+    case -1:
+      return `You Lose, ${HAND[cSelection]} beats ${HAND[pIndex]}.`;
+      break;
+    case 2:
+      return `You Lose, ${HAND[cSelection]} beats ${HAND[pIndex]}.`;
+      break;
+    case -2:
+      return `You Win, ${HAND[pIndex]} beats ${HAND[cSelection]}.`;
+      break;
+    default:
+      return "There are some issus with the logic :roll_eyes:";
+  }
 }
