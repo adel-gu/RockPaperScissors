de51e61ab90ab76dfea0377a3c041b5e7fe54b2c
adelguit <adeelguitoun@gmail.com>
2022-07-14 15:02:51 +0100

    gameResult halper function added.

--- app.js
+++ app.js
@@ -4,31 +4,34 @@
 const CHECKHAND = ["rock", "paper", "scissors"]; // for checking logic arguments and user inputs.
 const HAND = ["Rock", "Paper", "Scissors"]; // For showing a clean result to the user.
 
-// ? Main Function
+//? Main Function
 game();
 
-// ! Function Declarations
+//! Function Declarations
 function game() {
   let pScore = 0,
     cScore = 0;
   // play 5 rounds of game, and keep track of scores.
   for (let i = 0; i < 5; i++) {
     const computerSelection = computerPlay();
-    let playerSelection = prompt(
+    const playerSelection = prompt(
       "Choose an Option to save the world: Rock, Paper or Scissors !?"
     );
     if (playerSelection != null) {
-      let result = playRound(playerSelection, computerSelection);
-      if (result == 1) pScore++;
-      else cScore++;
+      let round = playRound(playerSelection, computerSelection);
+      if (round == 1) pScore++;
+      else if (round == -1) cScore++;
     }
   }
+  console.log(gameResult(pScore, cScore));
+}
 
+function gameResult(pScore, cScore) {
   if (pScore > cScore)
-    console.log(`You Scored ${pScore} points over ${cScore} points, You WIN!`);
+    return `You Scored ${pScore} points over ${cScore} points, You WIN!`;
   else if (pScore < cScore)
-    console.log(`You Scored ${pScore} points over ${cScore} points, You Lose!`);
-  else console.log("That's a Tie!!");
+    return `You Scored ${pScore} points over ${cScore} points, You Lose!`;
+  else return "That's a Tie!!";
 }
 
 function computerPlay() {
@@ -40,21 +43,22 @@ function playRound(pSelection, cSelection) {
   pIndex = CHECKHAND.indexOf(pSelection.toLowerCase());
   switch (pIndex - cSelection) {
     case 0:
-      return 0;
+      return 0; //* a Tie case.
       break;
-    case 1:
+    case 1: //* case (player VS computer) paper VS rock or scissors VS Paper. Player Win.
       return 1;
       break;
-    case -1:
+    case -1: //* case (player VS Computer ) rock VS paper or paper VS scissors => Player lose.
       return -1;
       break;
-    case 2:
+    case 2: //* case (player VS Computer ) scissors VS rock => Player lose.
       return -1;
       break;
-    case -2:
+    case -2: //* case (player VS Computer ) rock VS scissors => Player Win.
       return 1;
       break;
     default:
-      return "There are some issus with the logic :roll_eyes:";
+      return "There are some issus with the logic :roll_eyes: ";
+      break;
   }
 }
